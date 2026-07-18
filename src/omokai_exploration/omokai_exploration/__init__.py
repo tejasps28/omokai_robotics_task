1c6f9cc5e4e0015a972880665479b3a4c6917e28
tejasps28 <tejasps28@gmail.com>
2026-07-18 15:20:26 +0530

    Add deterministic frontier selection

--- src/omokai_exploration/omokai_exploration/__init__.py
+++ src/omokai_exploration/omokai_exploration/__init__.py
@@ -1,9 +1,21 @@
 """Autonomous exploration domain and ROS integration."""
 
+from .frontier import (
+    FrontierCandidate,
+    FrontierConfig,
+    frontier_cells,
+    frontier_clusters,
+    rank_frontiers,
+)
 from .grid import Cell, GridMetadata, OccupancyGrid
 
 __all__ = [
     'Cell',
+    'FrontierCandidate',
+    'FrontierConfig',
     'GridMetadata',
     'OccupancyGrid',
+    'frontier_cells',
+    'frontier_clusters',
+    'rank_frontiers',
 ]
